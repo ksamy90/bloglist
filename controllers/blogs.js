08b68320f131b657ec18a78434237cf5ef70f126
samson <samson.omondi1@ibm.com>
2023-04-29 23:10:35 +0300

    add routes for blogs and users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,30 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
+const getToken = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 blogsRouter.get("/", async (_request, response) => {
-  const blogs = await Blog.find({}).populate("user");
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
-  const blog = new Blog({ title: body.title, author: user.id, url: body.url });
+  const decodedToken = jwt.verify(getToken(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({ title: body.title, author: user._id, url: body.url });
 
   const savedBlog = await blog.save();
   user.blogs = user.blogs.concat(savedBlog);
--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ usersRouter.post("/", async (request, response) => {
 
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
-  const user = new User({ username, name, passwordHash });
+  const user = new User({ username, name, password: passwordHash });
 
   const savedUser = await user.save();
   response.status(201).json(savedUser);
